2ccc4446ef488f91cfae360099c9a8272680f8b2
Raj Kevadiya <kevadiyar98@gmail.com>
2022-11-26 11:53:27 +0530

    Initial commit

---
+++ app/src/main/java/com/raj1/progressbar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raj1.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
